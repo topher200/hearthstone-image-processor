0f44d727c5205e3a76e1266ae6259f2e010eaaee
Topher Brown <chris.brown@farsounder.com>
2014-03-07 00:25:33 -0500

    remove old main func

--- src/hearthstone_bot/core.clj
+++ src/hearthstone_bot/core.clj
@@ -32,29 +32,4 @@
   []
   (map find-and-draw-match all-cards))
 
-(defn -main
-  []
-  (info "---start---")
-  (let [
-        board-image-name (path-to-resource "croc_board.png")
-        ;; board-image-name (path-to-resource "boar_hand.png")
-        ;; card-image-name (path-to-resource "boar_card.png")
-        card-image-name (path-to-resource "croc_card.png")
-        board-image-color (cv/load-image board-image-name)
-        board-image (cv/gray-image board-image-color)
-        card-image (cv/gray-image (load-image card-image-name))
-        card-image-cropped (cv/crop-image card-image)
-        match-image (cv/template-match board-image card-image-cropped)
-        normalized-image (cv/normalize match-image)
-        match-location (cv/find-match-location match-image)
-        template-size (.size card-image-cropped)
-        ;; board-to-draw board-image-color
-        board-to-draw normalized-image
-        save-path (fs/path-to-resource "match.png")
-        ]
-    (info "checking for" card-image-name "on" board-image-name)
-    ;; (cv/draw-rectangle board-to-draw match-location template-size)
-    (cv/save-image board-to-draw save-path)))
-
 (find-and-draw-all-matches)
-;; (-main)
