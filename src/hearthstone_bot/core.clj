d29ed73f8b5dafbbd50e9d789a8bf31d5ac1fe17
Topher Brown <chris.brown@farsounder.com>
2014-03-06 20:19:14 -0500

    create find-and-draw-croc-match

--- src/hearthstone_bot/core.clj
+++ src/hearthstone_bot/core.clj
@@ -5,29 +5,38 @@
    )
   (:use [clojure.tools.logging :only (debug info warn error spy)]))
 
+(defn find-and-draw-croc-match
+  []
+  (let [game-image (load-image (fs/path-to-resource "croc_board.png"))
+        card-image (crop-image (load-image (fs/path-to-resource "croc_card.png")))
+        save-path (fs/path-to-resource "match.png")]
+    (draw-rectangle
+     game-image 
+     (cv/find-match-location (cv/template-match game-image card-image))
+     (.size card-image))
+    (cv/save-image game-image save-path)))
+
 (defn -main
   []
   (info "---start---")
-  (clojure.lang.RT/loadLibrary Core/NATIVE_LIBRARY_NAME)
   (let [
-        ;; ;; board-image-name "croc_board.png"
-        ;; board-image-name "boar_hand.png"
-        ;; card-image-name "boar_card.png"
-        ;; ;; card-image-name "croc_card.png"
-        ;; board-image-color (load-image board-image-name)
-        ;; board-image (gray-image board-image-color)
-        ;; card-image (gray-image (load-image card-image-name))
-        ;; card-image-cropped (crop-image card-image)
-        ;; match-image (template-match board-image card-image-cropped)
-        ;; normalized-image (normalize match-image)
-        ;; match-location (find-match-location match-image)
-        ;; template-size (.size card-image-cropped)
-        ;; ;; board-to-draw board-image-color
-        ;; board-to-draw board-image
-        ;; save-path (path-to-resource "match.png")
+        board-image-name (path-to-resource "croc_board.png")
+        ;; board-image-name (path-to-resource "boar_hand.png")
+        ;; card-image-name (path-to-resource "boar_card.png")
+        card-image-name (path-to-resource "croc_card.png")
+        board-image-color (cv/load-image board-image-name)
+        board-image (cv/gray-image board-image-color)
+        card-image (cv/gray-image (load-image card-image-name))
+        card-image-cropped (cv/crop-image card-image)
+        match-image (cv/template-match board-image card-image-cropped)
+        normalized-image (cv/normalize match-image)
+        match-location (cv/find-match-location match-image)
+        template-size (.size card-image-cropped)
+        ;; board-to-draw board-image-color
+        board-to-draw normalized-image
+        save-path (fs/path-to-resource "match.png")
         ]
-    (error (get-all-files fs/cards-directory))))
-    ;; (info "checking for" card-image-name "on" board-image-name)
-    ;; (draw-rectangle board-to-draw match-location template-size)
-    ;; (save-image board-to-draw save-path)))
-(-main)
+    (info "checking for" card-image-name "on" board-image-name)
+    ;; (cv/draw-rectangle board-to-draw match-location template-size)
+    (cv/save-image board-to-draw save-path)))
+(find-and-draw-croc-match)
